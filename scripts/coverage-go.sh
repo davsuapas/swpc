4d6d6a60190434a4fdc93fe4b82ffedcf710d619
David Suarez Pascual <dav.sua.pas@gmail.com>
2023-02-14 08:43:36 +0100

    Exclude emulator in coverage

--- scripts/coverage-go.sh
+++ scripts/coverage-go.sh
@@ -3,3 +3,8 @@
 # Coverage in GO
 
 go test -race -covermode=atomic -coverprofile=coverage.out -coverpkg ./... ./...
+
+# The emulator main is excluded because the emulator is a tool for testing the hub,
+# and it is not necessary to test on the emulator.
+grep -v "/cmd/swpc-emulator/main.go" ./coverage.out > ./coverage-final.out
+mv ./coverage-final.out ./coverage.out
